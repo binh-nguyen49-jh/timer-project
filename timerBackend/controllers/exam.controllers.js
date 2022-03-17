164b51a6ba875786534cebdaf129ebc51c427b36
binh-nguyen49-jh <binh.nguyen@journeyh.io>
2022-03-17 16:35:10 +0700

    remove redundant code in exam controller

--- timerBackend/controllers/exam.controllers.js
+++ timerBackend/controllers/exam.controllers.js
@@ -7,20 +7,11 @@ const getRandomExam = ({res, next}) => {
         
         const fileNames = fs.readdirSync(process.env.FILE_DIRECTORY)
         const randomNum = Math.floor(Math.random()*fileNames.length)
-        console.log(fileNames, randomNum)
-        const exam = fs.readFileSync(`${process.env.FILE_DIRECTORY}/exam2.json`, {encoding:'utf8', flag:'r'})
+        const exam = fs.readFileSync(`${process.env.FILE_DIRECTORY}/${fileNames[randomNum]}`, {encoding:'utf8', flag:'r'})
         if(exam)
         res.status(200).send( JSON.parse(exam))
         else
         next(createErrors.InternalServerError("Something went wrong!"))
-        // fs.readdirSync(dirPath).forEach(function(file) {
-        //     let filepath = path.join(dirPath , file);
-        //     let stat= fs.statSync(filepath);
-        //     if (stat.isDirectory()) {            
-        //       getAllFiles(filepath);
-        //     } else {
-        //           console.info(filepath+ '\n');                      
-        //     }    
     } catch (error) {
         console.error(error)
         next(createErrors.InternalServerError("Something went wrong!"))
