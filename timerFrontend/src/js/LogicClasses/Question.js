4fe1315c634c39c611b0f019faf2da6e122ebef4
binh-nguyen49-jh <binh.nguyen@journeyh.io>
2022-03-18 09:41:53 +0700

    fix code indentation & object rules in Question logic class

--- timerFrontend/src/js/LogicClasses/Question.js
+++ timerFrontend/src/js/LogicClasses/Question.js
@@ -1,4 +1,3 @@
-
 class Question{
   constructor({
     content, 
@@ -8,10 +7,10 @@ class Question{
     this.answer = answer;
     if (this.constructor === Question) {
       throw new Error("You cannot create an instance of Abstract Class");
-    }
+    };
     if (this.checkAnswer === undefined) {
       throw new TypeError('Override checkAnswer function');
-    }
+    };
   };
 }
 
@@ -32,7 +31,7 @@ export class MultiChoiceQuestion extends Question{
   checkAnswer(userAnswer) {
     if(!userAnswer) {
       return false;
-    }
+    };
     return this.answer === userAnswer;
   };
 }
@@ -53,18 +52,15 @@ export class TextQuestion  extends Question{
     if (!userAnswer) {
       return false;
     }
-    
     const normalizedAnswer = userAnswer.trim().toLowerCase();
     const userKeywords = normalizedAnswer.match(/[A-Za-z0-9]+/g);
-    
     const countKeywords = {}
     for (let word of userKeywords) {
       // Mark as user answer right keywords
       if (this.keywords.indexOf(word) !== -1) {
         countKeywords[word] = 1;
-      }
+      };
     }
-    
     let totalCorrectKeywords = 0;
     for (let word of userKeywords) {
       const isExist = countKeywords[word]? 1 : 0;
@@ -76,20 +72,20 @@ export class TextQuestion  extends Question{
 }
 
 export function loadQuestions(questions) {
-    const questionInstances = [];
-    for (let question of questions) {
-      if (question.choices) {
-        questionInstances.push(new MultiChoiceQuestion({
-          content: question.content,
-          answer: question.answer,
-          choices: question.choices
-        }));
-      } else {
-        questionInstances.push(new TextQuestion({
-          content: question.content,
-          answer: question.answer
-        }));
-      }
+  const questionInstances = [];
+  for (let question of questions) {
+    if (question.choices) {
+      questionInstances.push(new MultiChoiceQuestion({
+        content: question.content,
+        answer: question.answer,
+        choices: question.choices
+      }));
+    } else {
+      questionInstances.push(new TextQuestion({
+        content: question.content,
+        answer: question.answer
+      }));
     }
-    return questionInstances;
-  };
+  }
+  return questionInstances;
+};
